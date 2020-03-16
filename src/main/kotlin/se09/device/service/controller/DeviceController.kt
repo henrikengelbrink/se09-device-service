3f0662d526b7f59916c3e26c3698af8d9666df68
henrikengelbrink <henrik@keeet.io>
2020-03-16 22:59:06 +0100

    feat: Add claim endpoint

--- src/main/kotlin/se09/device/service/controller/DeviceController.kt
+++ src/main/kotlin/se09/device/service/controller/DeviceController.kt
@@ -4,30 +4,38 @@ import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Header
+import io.micronaut.http.annotation.Put
 import io.micronaut.http.server.types.files.SystemFile
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import se09.device.service.services.DeviceService
 import javax.inject.Inject
 
 @Controller("/devices")
 class DeviceController {
 
+    private val LOG: Logger = LoggerFactory.getLogger(DeviceController::class.java)
+
     @Inject
     private lateinit var deviceService: DeviceService
 
     @Get(produces = [MediaType.APPLICATION_JSON])
     fun createDevice(): HttpResponse<SystemFile> {
+        LOG.warn("########### createDevice")
         val zipFile = this.deviceService.createDevice()
         return HttpResponse.created(zipFile)
     }
 
-    @Get("/test", produces = [MediaType.APPLICATION_JSON])
-    fun test(): HttpResponse<Any> {
-        Runtime.getRuntime().exec("ls -la")
-        Runtime.getRuntime().exec("pwd")
-        Runtime.getRuntime().exec("cat /vault/secrets/chain.crt")
-        return HttpResponse.ok("")
+    @Put(produces = [MediaType.APPLICATION_JSON])
+    fun claimDevice(
+            @Header(value = "X-User-Id") userId: String
+    ): HttpResponse<Any> {
+        LOG.warn("########### claimDevice $userId")
+        val responseBody = mapOf(
+                "password" to "1234567890"
+        )
+        return HttpResponse.ok(responseBody)
     }
-
-
 
 }
