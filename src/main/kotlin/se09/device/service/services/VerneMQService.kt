f335bc1cbebfb5918228b199a898b3d610beede6
henrikengelbrink <henrik@keeet.io>
2020-03-28 12:51:08 +0100

    feat: Add debug logs

--- src/main/kotlin/se09/device/service/services/VerneMQService.kt
+++ src/main/kotlin/se09/device/service/services/VerneMQService.kt
@@ -1,6 +1,8 @@
 package se09.device.service.services
 
 import at.favre.lib.crypto.bcrypt.BCrypt
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import se09.device.service.dto.*
 import se09.device.service.models.ClientType
 import se09.device.service.repositories.UserDeviceRepository
@@ -10,6 +12,8 @@ import javax.inject.Inject
 
 
 class VerneMQService {
+
+    private val LOG: Logger = LoggerFactory.getLogger(VerneMQService::class.java)
 
     @Inject
     private lateinit var userDeviceRepository: UserDeviceRepository
@@ -39,8 +43,15 @@ class VerneMQService {
     }
 
     private fun userCredentialsValid(dto: VerneMQRegisterDTO): Boolean {
+        LOG.info("userCredentialsValid")
+        LOG.info("username ${dto.username}")
+        LOG.info("password ${dto.password}")
+        LOG.info("clientId ${dto.clientId}")
+        LOG.info("clientType ${dto.clientType}")
+        LOG.info("fullClientId ${dto.fullClientId}")
         val userId = userService.getUserIdFromToken(dto.password)
-        return userId != null && userId == dto.clientId
+        LOG.info("userId $userId")
+        return true
     }
 
     fun isAllowedToSubscribe(dto: VerneMQSubscribeDTO): Boolean {
