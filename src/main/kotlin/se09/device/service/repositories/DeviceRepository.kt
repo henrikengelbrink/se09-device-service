9a38e3ea05c9c6759444be5e59e392967248f40c
henrikengelbrink <henrik@keeet.io>
2020-03-29 22:52:43 +0200

    fix: Change entity

--- src/main/kotlin/se09/device/service/repositories/DeviceRepository.kt
+++ src/main/kotlin/se09/device/service/repositories/DeviceRepository.kt
@@ -3,12 +3,11 @@ package se09.device.service.repositories
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.repository.CrudRepository
 import se09.device.service.models.Device
-import se09.device.service.models.UserDevice
 import java.util.*
 
 @Repository
 interface DeviceRepository : CrudRepository<Device, String> {
 
-    fun findByUserIdAndDeletedAtIsNull(userId: UUID): List<UserDevice>
+    fun findByUserIdAndDeletedAtIsNull(userId: UUID): List<Device>
 
 }
